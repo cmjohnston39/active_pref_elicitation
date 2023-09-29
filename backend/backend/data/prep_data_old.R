9ea5074180a09296a81da9b91d47ee72780f7cd7
Caroline Johnston <cmjohnst@usc.edu>
2023-09-28 21:04:46 -0700

    add more files

--- backend/backend/data/prep_data_old.R
+++ backend/backend/data/prep_data_old.R
@@ -10,6 +10,13 @@ rescale01 <- function(x) {
     (x - rng[1]) / (rng[2] - rng[1])
 }
 
+# rescaleneg11 <- function(x) {
+#     mn <- mean(x, na.rm = TRUE)
+#     n <- length(x)
+#     std <- sqrt((n-1)/n) * sd(x, na.rm = TRUE)
+#     (x - mn) / std
+# }
+
 for(file in policy_csvs){
     data <- read_csv(glue::glue("COVID/{file}.csv"))
     # generate data to be displayed in app
@@ -39,5 +46,40 @@ for(file in policy_csvs){
         write_json(path = glue::glue("COVID/{file}1.json"))
     
 }
+
+policy_csvs <- c("AdultHMIS_20210922_preprocessed_final_Robust_edit")
+
+for(file in policy_csvs){
+  data <- read_csv(glue::glue("LAHSA/{file}.csv"))
+  #drop columns 
+  data <- subset(data,select = -c(NumDatapoints, TreeDepth, BranchingLimit, TimeLimit, SolverStatus, ObjVal,
+               MIPGap, SolvingTime, NumBranchingNodes))
+  # generate data to be displayed in app
+  data %>%
+    rowid_to_column("policy_id") %>%
+    mutate(policy_id = policy_id - 1) %>% 
+    select(-one_of("Approach")) %>% 
+    pivot_longer(-one_of("policy_id"),names_to = "labels", values_to = "values") %>% 
+    split(.$policy_id) %>%
+    map(~  list(labels = .x$labels, values = .x$values)) %>% 
+    write_json(path = glue::glue("LAHSA/{file}.json"), auto_unbox=T)
+  # generate -1-1 standardized data to use in backend
+  data  %>% 
+    mutate(across(where(is.numeric), rescale01)) %>%
+    rowid_to_column("policy_id") %>% 
+    mutate(policy_id = policy_id - 1) %>% 
+    select(-one_of("Approach")) %>% 
+    pivot_longer(-one_of("policy_id"),names_to = "labels", values_to = "values") %>% 
+    split(.$policy_id) %>%
+    map(~  list(labels = .x$labels, values = .x$values)) %>% 
+    write_json(path = glue::glue("LAHSA/{file}_rescaled01.json"), auto_unbox=T)
+  data %>% pivot_longer(-one_of("Approach"),names_to = "labels", values_to = "values") %>%
+    rename(policy_id = Approach) %>% group_by(policy_id) %>%
+    group_map(.f=function(data,...){
+      list(policy_id=...$policy_id, labels=data$labels, values=data$values)
+    }) %>%
+    write_json(path = glue::glue("LAHSA/{file}1.json"))
+  
 }
+
  
